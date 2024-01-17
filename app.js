2ca7037d1110648bedb5f231259986209f5d1181
Bahaa <bahaa.bakri1995@gmail.com>
2024-01-17 18:50:43 +0400

    Mongoose: implement user model

--- app.js
+++ app.js
@@ -19,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById('5baa2528563f16379fc8a610')
-  //   .then(user => {
-  //     req.user = new User(user.name, user.email, user.cart, user._id);
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next()
+  User.findOne()
+    .then(user => {
+      req.user = user
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -34,8 +33,23 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 connectWithMongoose.then(() => {
+  console.log('Connected')
   app.listen(3000);
-  console.log('CONNECTED !!!')
+  // create user or get saved one
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name:'Bahaa',
+        email:'bahaa@gmail.com',
+        cart: {
+          items: []
+        }
+      })
+      user.save()
+    }
+    
+  })
+
 })
 .catch(err => {
   console.error(err)
--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const productSchema = mongoose.Schema({
+const productSchema = new mongoose.Schema({
   title: {
     type: String,
     required:true
